503e424f2294bcbfc975a3104e0dd2e3e381c7ed
Subindo a recuperacao para o Git <caio.p.silva8@senail.aluno.br>
2025-04-24 08:28:47 -0300

    feat: subindo

---
+++ Projeto React Filme/filmes/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import CadastroFilme from './pages/cadastroFilme/CadastroFilme';
+// import Login from "./pages/login/Login";
+
+function App() {
+  return (
+    <>
+      {/* chamar as paginas */}
+      {/* <Login/> */}
+      <CadastroFilme/>
+    </>
+  );
+}
+
+
+export default App;
